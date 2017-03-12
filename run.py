067c50d34849185e6e40bdb8626ffbe64bbf03c3
Dmitry Birin <dmitry.birin@gmail.com>
2017-03-12 23:23:49 +0300

    run.py refactoring

--- run.py
+++ run.py
@@ -1,13 +1,18 @@
-from flask import Flask, request, jsonify, current_app
+import os
+from flask import Flask, request, jsonify, send_from_directory
 from gpio import Flash, Status
 from ServerExceptions import BadRequestException
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='static')
 app.debug = True
 
 @app.route("/")
-def hello():
+def root():
     return app.send_static_file('index.html')
+
+@app.route('/<path:filename>')  
+def send_file(filename):  
+    return send_from_directory(app.static_folder, filename)
 
 @app.route("/colors/", methods=["PUT"])
 def switch_pin():
